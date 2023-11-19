90847e5c87cf578197421358e3efa6e8cd53ead6
Thimo812 <114487448+Thimo812@users.noreply.github.com>
2023-11-19 14:08:01 -0800

    startpagina en register pagina front end gemaakt

--- MatchingAppWindow/MainWindow.xaml.cs
+++ MatchingAppWindow/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MatchingAppWindow.Views;
+using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,18 @@ namespace MatchingAppWindow
         public MainWindow()
         {
             InitializeComponent();
+
+            var startScreen = new StartScreen();
+
+            startScreen.RegisterButton.Click += SwitchToRegisterScreen;
+
+            Content = startScreen;
+
+        }
+
+        private void SwitchToRegisterScreen(Object? sender, EventArgs args)
+        {
+            Content = new RegisterScreen();
         }
     }
 }
